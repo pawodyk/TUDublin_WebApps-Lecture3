a3a664c22adba71dbebeed7552c84b2449b6b815
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-02-29 15:03:13 +0000

    completed ex2

---
+++ Boot-mvc/boot2/contactDetails.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+
+    <title>MGW: Contact Details</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+
+<body class="container">
+
+<nav>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link" href="/index.php">home</a>
+        </li>
+        <li>
+            <a class="nav-link active" href="/contactDetails.php">contact details</a>
+        </li>
+    </ul>
+</nav>
+
+<h1>MGW Contact Details</h1>
+<p>
+    email: info@my-great-website.com<br>
+    Phone: +353 1 888 2233<br>
+    Post: 198 Main Street, Dublin, Ireland
+</p>
+
+</body>
+</html>
---
+++ Boot-mvc/boot2/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+
+    <title>MGW Home Page</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+
+<body class="container">
+
+<nav>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link active" href="/index.php">home</a>
+        </li>
+        <li>
+            <a class="nav-link" href="/contactDetails.php">contact details</a>
+        </li>
+    </ul>
+</nav>
+
+<h1>Welcome to MGW</h1>
+<p>Great products at great prices!</p>
+
+</body>
+</html>
