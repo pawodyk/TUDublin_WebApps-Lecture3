8df6b280e48d7691511ce41fdc61da4c7d53b4ac
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-02-29 15:03:05 +0000

    completed ex1

---
+++ Boot-mvc/boot1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MGW Home Page</title>
+</head>
+
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="/index.php">home</a></li>
+            <li><a href="/contactDetails.php">contact details</a></li>
+        </ul>
+    </nav>
+
+    <h1>Welcome to MGW</h1>
+    <p>Great products at great prices!</p>
+
+</body>
+</html>
