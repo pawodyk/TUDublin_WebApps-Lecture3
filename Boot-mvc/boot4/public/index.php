d37f09a2b7aaf452f82813efca7ea6b8de913fe3
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-03-01 19:10:35 +0000

    added composer atutololoader to ex4

--- Boot-mvc/boot4/public/index.php
+++ Boot-mvc/boot4/public/index.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../src/WebApplication.php';
-require_once __DIR__ . '/../src/MainController.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use TUDublin\WebApplication;
 
